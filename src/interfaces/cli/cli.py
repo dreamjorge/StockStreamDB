f8d7fec98b311ea674693775d4967ccff5dd7306
Jorge Ugalde <jorge-kun@live.com>
2024-09-23 17:49:09 -0600

    feat: Add log component

--- src/interfaces/cli/cli.py
+++ src/interfaces/cli/cli.py
@@ -7,8 +7,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.
 import argparse
 from src.application.use_cases.collect_stock_data import CollectStockData
 from src.infrastructure.db.stock_repository import StockRepository
-
+from src.utils.logging_config import setup_logging
 def main():
+    
+    setup_logging()
     parser = argparse.ArgumentParser(
         description="CLI for collecting financial data: stocks, cryptocurrencies, and news"
     )
