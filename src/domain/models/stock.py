36d719a53dd0667afffc1d646b55554e82d000a6
Jorge Ugalde <jorge-kun@live.com>
2024-09-24 11:50:38 -0600

    feat: files for implement CRUD are added

--- src/domain/models/stock.py
+++ src/domain/models/stock.py
@@ -1,5 +1,5 @@
 class Stock:
-    def __init__(self, ticker: str, close_price: float, date: str):
+    def __init__(self, ticker, name, sector):
         self.ticker = ticker
-        self.close_price = close_price
-        self.date = date
+        self.name = name
+        self.sector = sector
