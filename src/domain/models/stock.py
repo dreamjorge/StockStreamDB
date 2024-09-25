71e0ec0b8cfef7fb7cb396bffaaee014872c7ad0
Jorge Ugalde <jorge-kun@live.com>
2024-09-25 11:28:36 -0600

    fix(test): modify stock class and test for success on test

--- src/domain/models/stock.py
+++ src/domain/models/stock.py
@@ -1,5 +1,6 @@
 class Stock:
-    def __init__(self, ticker, name, sector):
+    def __init__(self, ticker, name, sector, close_price):
         self.ticker = ticker
         self.name = name
         self.sector = sector
+        self.close_price = close_price
